6d4317336ac2ea62f1530f4bd8b0bb768cc42d34
Testrithub <145240472+Testrithub@users.noreply.github.com>
2023-09-19 12:50:55 +0530

    Create Addition.c

---
+++ Addition.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+- int main(){
+
+int stu;
+
+int cla;
+
+printf("enter a number ");
+
+
+ scanf("%d", &cla);
+
+
+
+printf("enter a number ");
+
+
+
+scanf("%d", &stu);
+
+ int add = cla+stu;
+
+
+
+printf("addition is %d", add);
+}
